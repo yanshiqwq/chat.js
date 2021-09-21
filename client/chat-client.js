af0f4fd063d32a3a1e000eb98a652784013140cd
Yanshiqwq <yanshiqwq@outlook.com>
2021-09-21 22:42:06 +0800

    v1.1

--- client/chat-client.js
+++ client/chat-client.js
@@ -5,25 +5,7 @@ var server = 'ws://localhost:3272/';
 
 // Define
 
-const chalk = require('chalk');
-const async = require('async');
-const readline = require('readline');
-const error = function(log){console.error(chalk.bold.red(log))};
-const warn = function(log){console.warn(chalk.bold.yellow(log))};
-const log = function(log){console.log(chalk.bold.cyan(log))};
-const info = function(log){console.info(chalk.bold.greenBright(log))};
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-rl.setPrompt('> ');
-const emptyLine = function(callback){
-	readline.clearLine(process.stdout, 0, function(){
-		readline.cursorTo(process.stdout, 0, function(){
-			callback();
-		});
-	});
-}
+const {app, BrowserWindow} = require('electron')
 var wsc = require('websocket').client;
 var client = new wsc();
 const gettime = function(){
@@ -42,75 +24,53 @@ const gettime = function(){
 	}else{
 		var seconds=new Date().getSeconds();
 	}
-	return "["+hours+":"+minutes+":"+seconds+"] ";
+	return [hours, minutes, seconds];
 }
 
 // Client
 
 var connection;
 client.on('connectFailed', function(err){
-	emptyLine(function(){
-		error(gettime() + '[ERROR] Connection error: ' + err.toString());
-		rl.prompt();
-	});
 	client.connect(server);
 });
 client.on('connect', function(conn){
-	connection = conn;
-	emptyLine(function(){
-		log(gettime() + '[INFO] Successfuly connected.');
-		rl.prompt();
-	});
 	conn.on('error', function(err) {
-		emptyLine(function(){
-			error(gettime() + "[ERROR] Connection error: " + err.toString());
-			rl.prompt();
-		});
-	});
-	conn.on('close', function() {
-		emptyLine(function(){
-			log(gettime() + '[INFO] Connection closed.');
-			rl.prompt();
-		});
 		client.connect(server);
 	});
-	conn.on('message', function(message) {
-		emptyLine(function(){
-			messageJson = JSON.parse(message.utf8Data);
-			if(messageJson['code'] == 200){
-				switch(messageJson['type']){
-					case 'login':
-						info(gettime() + 'Successfuly login, token: ' + messageJson['token']);
-						break;
-					case 'message':
-						log(gettime() + '<' + messageJson['token'] + '> ' + messageJson['message'][1]);
-						break;
-					case 'newMember':
-						log(gettime() + messageJson['token'] + ' joined the chat room.');
-						break;
-					default:
-						error(gettime() + 'Unknown message type.');
-				}
-			}
-			rl.prompt();
-		});
+	conn.on('close', function() {
 	});
-});
-function main(){
-	rl.question('> ', function(input){
-		var client_message = JSON.stringify({method: "send_message", message: input});
-		try{
-			connection.send(client_message);
-		}catch(err){
-			if(err == "TypeError: Cannot read property 'send' of undefined"){
-				error(gettime() + "[ERROR] Not connected to any server. ");
-			}else{
-				error(gettime() + "[ERROR] Client error: " + err.toString());
+	conn.on('message', function(message) {
+		messageJson = JSON.parse(message.utf8Data);
+		if(messageJson['code'] == 200){
+			switch(messageJson['type']){
+				case 'login':
+					info(gettime() + 'Successfuly login, token: ' + messageJson['token']);
+					break;
+				case 'message':
+					log(gettime() + '<' + messageJson['token'] + '> ' + messageJson['message'][1]);
+					break;
+				case 'newMember':
+					log(gettime() + messageJson['token'] + ' joined the chat room.');
+					break;
+				default:
+					error(gettime() + 'Unknown message type.');
 			}
 		}
-		main();
 	});
-}
-log(gettime() + '[INFO] Connecting to the server...')
+});
 client.connect(server);
-main();
+function createWindow(){   
+  // 创建浏览器窗口
+  
+  // 加载index.html文件
+  win.loadFile('web/index.html');
+}
+app.on('ready', function(){
+	let win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+});
--- server/chat-server.js
+++ server/chat-server.js
@@ -1,24 +1,25 @@
 
 // Define
 
+const cookieParser = require('cookie-parser');
+const svgCaptcha = require('svg-captcha');
 const readline = require('readline');
+const express = require('express');
 const crypto = require('crypto');
 const chalk = require('chalk');
 const http = require('http');
+const url = require('url')
 const ws = require('ws');
-const cli = require('minimist');
+const app = express();
 
 const error = function(log){console.error(chalk.bold.red(log))};
 const warn = function(log){console.warn(chalk.bold.yellow(log))};
 const log = function(log){console.log(chalk.bold.cyan(log))};
 const info = function(log){console.info(chalk.bold.greenBright(log))};
-
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-rl.setPrompt('> ');
-const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 const emptyLine = function(callback){
 	readline.clearLine(process.stdout, 0, function(){
 		readline.cursorTo(process.stdout, 0, function(){
@@ -62,19 +63,55 @@ const getTime = function(){
 
 // Config
 
-var port = 3272;
+var wsPort = 3272;
+var httpPort = 3273;
 
 // Server
 
-server = new ws.Server({port: port});
-server.on('connection',function(ws, req){
+rl.setPrompt('> ');
+captchaList = {};
+connectionList = {};
+app.use(cookieParser());
+app.get('/captcha',(req,res)=>{
+	var uuid = uuid();
+    var captcha = svgCaptcha.create({
+        color: true,
+        inverse:false,
+        width: 100,
+        height: 40,
+        fontSize: 48,
+        size: 4,
+        noise: 3,
+        ignoreChars: '0oO1ilI'
+    });
+    captcha.text.toLowerCase();
+    emptyLine(function(){log(getTime() + '[INFO] New captcha: ' + captcha.text)});
+    res.json({captcha: captcha.data, id: uuid});
+    captchaList[uuid] = captcha;
+})
+app.post('/register', function(req, res){
+	if(req.body.id & req.body.key & req.body.captcha){
+		if(captchaList[req.body.captcha[0]] == req.body.captcha[1]){
+			var json = {code: 200, time: timeStamp()};
+			res.json(json);
+		}else{
+			var json = {code: 400, time: timeStamp(), message: 'Validation failed'};
+			res.json(json);
+		}
+	}else{
+		var json = {code: 400, time: timeStamp(), message: 'Bad request'};
+		res.json(json);
+	}
+	res.json
+});
+app.listen(httpPort);
+
+wss = new ws.Server({port: wsPort, verifyClient: verify});
+wss.on('connection', function(ws, req){
 	ws.token = crypto.createHash('md5').update(req.headers['sec-websocket-key']).digest('hex').slice(1,8);
+	connectionList[ws.token] = ws;
 	emptyLine(function(){
-		log(getTime() + '[INFO] New connection: ' + ws.token)
-		var server_message = JSON.stringify({code: 200, time: timeStamp(), type: 'newMember', token: ws.token});
-		server.clients.forEach(function(client){
-			client.send(server_message);
-		});
+		log(getTime() + '[INFO] New connection: ' + ws.token);
 		rl.prompt();
 	});
 	var server_message = JSON.stringify({code: 200, time: timeStamp(), type: 'login', token: ws.token});
@@ -90,41 +127,60 @@ server.on('connection',function(ws, req){
 			emptyLine(function(){
 				try{
 					log(getTime() + '[INFO] ' + ws.token + ' > ' + raw);
-					if(message['method'] == 'send_message'){
-						var server_message = JSON.stringify({code: 200, token: ws.token, time: timeStamp(), type: 'message', message: ['text', message['message']]});
-						server.clients.forEach(function(client){
-							client.send(server_message);
-						});
-					}else if(message['method'] == 'send_file'){
-						var server_message = JSON.stringify({code: 200, token: ws.token, time: timeStamp(), type: 'message', message: ['file', message['file']]});
-						server.clients.forEach(function(client){
-							client.send(server_message);
-						});
-					}else if(message['method'] == 'send_image'){
-						var server_message = JSON.stringify({code: 200, token: ws.token, time: timeStamp(), type: 'message', message: ['image', message['image']]});
-						server.clients.forEach(function(client){
-							client.send(server_message);
-						});
-					}else{
-						var server_message = JSON.stringify({code: 400, token: ws.token, time: timeStamp(), type: 'error', message: 'Unknown method'});
-						ws.send(server_message);
-						warn(getTime() + '[WARN] > ' + server_message);
+					switch(message['method']){
+						case 'login':
+							ws.id = message['profile'][0];
+							ws.key = message['profile'][1];
+							connectionList[ws.token] = ws;
+							info(getTime() + ws.id + ' joined the chat room.');
+							var server_message = JSON.stringify({code: 200, time: timeStamp(), type: 'userJoin', id: ws.id});
+							wss.clients.forEach(function(client){
+								client.send(server_message);
+							});
+							break;
+						case 'message':
+							var server_message = JSON.stringify({code: 200, id: ws.id, time: timeStamp(), type: 'message', message: ['text', message['message']]});
+							try{
+								log(getTime() + '<' + ws.id + '> ' + message['message']);
+								wss.clients.forEach(function(client){
+									client.send(server_message);
+								});
+							}catch(err){
+								console.dir(err)
+							}
+							break;
+						default:
+							var server_message = JSON.stringify({code: 400, id: ws.id, token: ws.token, time: timeStamp(), type: 'error', message: 'Invalid method'});
+							ws.send(server_message);
+							warn(getTime() + '[WARN] ' + ws.token + ' > INVALID_METHOD: ' + message['method']);
 					}
 					rl.prompt();
 				}catch(err){
 					var server_message = JSON.stringify({code: 500, time: timeStamp(), type: 'error', message: err.message});
 					ws.send(server_message);
-					error(getTime() + '[ERROR] > ' + server_message);
+					emptyLine(function(){
+						error(getTime() + '[ERROR] > ' + server_message);
+					});
 					rl.prompt();
 				}
 			});
 		}
     })
     ws.on('close', function(code, reason){
+		delete connectionList[ws.token];
         emptyLine(function(){
 			log(getTime() + '[INFO] Connection closed: ' + ws.token);
+			try{
+				info(getTime() + ws.id + ' left the chat room.');
+			}catch(err){
+				console.dir(err);
+			}
 			rl.prompt();
 		});
+		var server_message = JSON.stringify({code: 200, time: timeStamp(), type: 'userQuit', id: ws.id});
+		wss.clients.forEach(function(client){
+			client.send(server_message);
+		});		
     });
     ws.on('error', function(code, reason){
 		emptyLine(function(){
@@ -133,26 +189,61 @@ server.on('connection',function(ws, req){
 		});
     });
 });
+function verify(info){
+	var ret = false;
+    var params = url.parse(info.req.url, true).query;
+    if (params["id"] == "yanshiqwq" && params["key"] == "minecraft666") {
+        ret = true;
+    }
+    return ret;  
+}
 function main(){
 	rl.question('> ', function(input){
-		switch(input.split(' ')[0]){
+		var argv = input.split(' ')
+		switch(argv[0]){
 			case 'list':
 				var clientTokens = [];
-				server.clients.forEach(function(client){
+				wss.clients.forEach(function(client){
 					clientTokens.push(client.token)
 				});
 				log("There are " + clientTokens.length + " clients in the list:")
 				info(clientTokens.join(', '));
 				break;
 			case 'kick':
-				
+				try{
+					connectionList[argv[1]].close();
+				}catch(err){
+					if(err == 'TypeError: Cannot read property \'close\' of undefined'){
+						var kicked = false;
+						wss.clients.forEach(function(client){
+							if(client.id == argv[1]){
+								client.close();
+								kicked = true;
+							}
+						});
+					}else{
+						emptyLine(function(){
+							error(getTime() + '[ERROR] Failed to close connection ' + argv[1] + ' : ' + err);
+							rl.prompt();
+						});
+					}
+				}
+				if(kicked == false){
+					emptyLine(function(){
+						error(getTime() + '[ERROR] User or connection not exists: ' + argv[1]);
+						rl.prompt();
+					});
+				}
+				break;
+			case 'stop':
+				process.exit();
 			default:
-				error("Invalid command.");
+				error(getTime() + "[ERROR] Invalid command.");
 				break;
 		}
 		main();
 	})
 }
 
-log(getTime() + '[INFO] Server running at 0.0.0.0:' + port + '.');
+log(getTime() + '[INFO] Server running at 0.0.0.0:' + wsPort + '.');
 main();
